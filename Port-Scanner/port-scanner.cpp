d33448e9553b057ee2a95951ad74aaa4ecdcbb3c
akash2061 <lucifermorningstar2061@gmail.com>
2024-05-28 20:03:17 +0530

    Invalid-Ports

--- Port-Scanner/port-scanner.cpp
+++ Port-Scanner/port-scanner.cpp
@@ -6,7 +6,17 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 #include <unistd.h> // for close
+
+#define MAX 65535
+
 using namespace std;
+
+bool is_valid_ip(const string &ip)
+{
+    struct sockaddr_in ipv4;
+    int result = inet_pton(AF_INET, ip.c_str(), &(ipv4.sin_addr));
+    return result != 0;
+}
 
 bool is_port_open(const string &ip, int port)
 {
@@ -33,42 +43,58 @@ bool is_port_open(const string &ip, int port)
 int main()
 {
     string ip;
-    int start_port, end_port;
+    unsigned int start_port, end_port; // Change the data type to unsigned int
 
     cout << "Enter IP address to scan: ";
     cin >> ip;
-    cout << "Enter start port: ";
+    if (!is_valid_ip(ip))
+    {
+        cerr << "Invalid IP address." << endl;
+        return 1;
+    }
+
+    cout << "Enter start port (0-65535): ";
     cin >> start_port;
-    cout << "Enter end port: ";
+    if (start_port < 0 || start_port > MAX)
+    {
+        cerr << "Invalid start port." << endl;
+        return 1;
+    }
+
+    cout << "Enter end port (0-65535): ";
     cin >> end_port;
+    if (end_port < 0 || end_port > MAX || end_port < start_port)
+    {
+        cerr << "Invalid end port." << endl;
+        return 1;
+    }
+
     cout << endl;
 
-    int o = 0, c = 0;
-    vector<int> open;
-    vector<int> close;
-    for (int port = start_port; port <= end_port; ++port)
+    int open_count = 0, closed_count = 0;
+    vector<int> open_ports;
+    for (unsigned int port = start_port; port <= end_port; ++port)
     {
         if (is_port_open(ip, port))
         {
-            o++;
-            open.push_back(port);
+            open_count++;
+            open_ports.push_back(port);
             cout << "Port " << port << " is open." << endl;
         }
         else
         {
-            c++;
-            close.push_back(port);
+            closed_count++;
             cout << "Port " << port << " is closed." << endl;
         }
     }
     cout << endl;
-    cout << "Open Ports: " << o << " :  [ ";
-    for (auto i : open)
+    cout << "Open Ports: " << open_count << " :  [ ";
+    for (auto port : open_ports)
     {
-        cout << i << " ";
+        cout << port << " ";
     }
     cout << "]" << endl;
-    cout << "Close Ports: " << c << endl;
+    cout << "Closed Ports: " << closed_count << endl;
 
     return 0;
 }
