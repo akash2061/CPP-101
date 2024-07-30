ae8d1f3849f67807c9274fec4d2fb17f4aa4b955
akash2061 <lucifermorningstar2061@gmail.com>
2024-07-30 22:44:50 +0530

    linked_list_loop_detectro

---
+++ Learning-STL/linked_list_loop_detectro.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node *next;
+
+    Node(int val)
+    {
+        data = val;
+        next = nullptr;
+    }
+};
+
+void loopHere(Node *head, Node *tail, int position)
+{
+    if (position == 0)
+        return;
+
+    Node *walk = head;
+    for (int i = 1; i < position; i++)
+        walk = walk->next;
+    tail->next = walk;
+}
+
+class Solution
+{
+public:
+    bool detectLoop(Node *head)
+    {
+        Node *temp = head;
+        map<Node *, int> m;
+
+        while (temp != nullptr)
+        {
+            if (m[temp] > 0)
+            {
+                return true;
+            }
+            m[temp]++;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    cin.ignore();
+
+    while (t--)
+    {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+
+        while (ss >> number)
+        {
+            arr.push_back(number);
+        }
+
+        int pos;
+        cin >> pos;
+        cin.ignore();
+
+        Node *head = nullptr;
+        Node *tail = nullptr;
+
+        if (!arr.empty())
+        {
+            head = new Node(arr[0]);
+            tail = head;
+            for (size_t i = 1; i < arr.size(); ++i)
+            {
+                tail->next = new Node(arr[i]);
+                tail = tail->next;
+            }
+            loopHere(head, tail, pos);
+        }
+
+        Solution ob;
+        if (ob.detectLoop(head))
+            cout << "true\n";
+        else
+            cout << "false\n";
+
+        unordered_set<Node *> visited;
+        Node *current = head;
+        while (current && visited.find(current) == visited.end())
+        {
+            visited.insert(current);
+            Node *temp = current;
+            current = current->next;
+            delete temp;
+        }
+    }
+
+    return 0;
+}
