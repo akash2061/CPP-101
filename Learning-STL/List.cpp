3f905361da11cc2912ca1d03cd4e075cfd64198a
akash2061 <lucifermorningstar2061@gmail.com>
2024-08-04 21:20:15 +0530

    List

--- Learning-STL/List.cpp
+++ Learning-STL/List.cpp
@@ -22,5 +22,37 @@ int main()
 
     cout << "List:" << endl;
     disp(l);
+
+    cout << "Pop-Back & Pop-Front:" << endl;
+    l.pop_back();
+    l.pop_front();
+    disp(l);
+
+    cout << "Enter Element of List to be poped:" << endl;
+    int x;
+    cin >> x;
+    l.remove(x);
+    disp(l);
+    cout << "Last Element of List:" << l.back();
+
+    list<int> l2;
+    cout << "\n\nEnter 5 Element to 2nd List:" << endl;
+    for (int i = 0, x; i < 5; i++)
+    {
+        cin >> x;
+        l2.push_back(x);
+    }
+    cout << "Sorted 2nd List:" << endl;
+    l2.sort();
+    disp(l2);
+
+    cout << "\nMerge List 1 & List 2:" << endl;
+    l.merge(l2);
+    disp(l);
+
+    cout << "Reverse List:" << endl;
+    l.reverse();
+    disp(l);
+
     return 0;
 }
